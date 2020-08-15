d1b9d8d60f5c771610c9b499fb2919e891fc773d
aminul007bd <gem007bd@gmail.com>
2020-08-15 14:09:32 +0200

    add highchart with json data

--- trip-highchart-example/src/App.tsx
+++ trip-highchart-example/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import HighChartAverageTripofDay from './components/HighChartAverageTripofDay';
+import { HighChartAverageTripofDay } from './components/HighChartAverageTripofDay';
 import HelloWorld from './components/HelloWorld';
 import './App.css';
 
+import bikeTripData from '../src/data/bike_trip_data.json';
+
 function App() {
+  const bikeData = { bikeTripData };
   return (
     <div className='App'>
-      <HighChartAverageTripofDay />
+      <HighChartAverageTripofDay bikeData={bikeData.bikeTripData} />
       <HelloWorld />
     </div>
   );
--- trip-highchart-example/src/components/HighChartAverageTripofDay.tsx
+++ trip-highchart-example/src/components/HighChartAverageTripofDay.tsx
@@ -1,5 +1,68 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
-export default function HighChartAverageTripofDay() {
-  return <div>This is the HighChart</div>;
-}
+export const HighChartAverageTripofDay = (bikeData: any) => {
+  // Define the necessary states for our chart
+  const [data] = useState(bikeData.bikeData);
+  const [bikeId] = useState(21434);
+  // Filter by a certain Bike ID
+  const filteredTrips = data.filter((n: any) => {
+    return n.bike === bikeId;
+  });
+
+  // HDD - Debug
+  // console.log("filteredTrips", filteredTrips);
+
+  // Format the series in a way that Highcharts understands
+  // Resource -- https://www.youtube.com/watch?v=G3BS3sh3D8Q
+  // Make sure that you adhere to creating a collection of arrays within 1 array.
+  const ready = [
+    {
+      name: 'Trips Count',
+      data: [filteredTrips.length]
+    }
+  ];
+
+  // Make the chart object and pass our `formattedSeries` array to it.
+  const chart = useMemo(() => {
+    return {
+      title: {
+        text: `Trips Count for One Bike`
+      },
+      subtitle: {
+        text: `trip count`
+      },
+      chart: {
+        type: 'bar'
+      },
+      yAxis: {
+        title: {
+          text: `Count of trips`
+        }
+      },
+      xAxis: {
+        title: {
+          text: ``
+        },
+        categories: [`Bike #${bikeId}`],
+        // I guess this is for visually impaired users.
+        accessibility: {
+          rangeDescription: 'Range: 2010 to 2017'
+        }
+      },
+      legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+      },
+      series: ready
+    };
+  }, [ready, bikeId]);
+
+  // HDD - Debug
+  console.log('chart', chart);
+
+  // Render the chart
+  return <HighchartsReact highcharts={Highcharts} options={chart} />;
+};
