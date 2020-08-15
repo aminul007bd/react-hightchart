f2361b7ddce2e9b2c3456599f1e74a8924dacdcb
aminul007bd <gem007bd@gmail.com>
2020-08-15 14:33:15 +0200

    add router

--- trip-highchart-example/src/App.tsx
+++ trip-highchart-example/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { HighChartAverageTripofDay } from './components/HighChartAverageTripofDay';
 import HelloWorld from './components/HelloWorld';
 import './App.css';
@@ -8,10 +9,25 @@ import bikeTripData from '../src/data/bike_trip_data.json';
 function App() {
   const bikeData = { bikeTripData };
   return (
-    <div className='App'>
-      <HighChartAverageTripofDay bikeData={bikeData.bikeTripData} />
-      <HelloWorld />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/dayTrip'>DayTrip</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path='/dayTrip'>
+            <HighChartAverageTripofDay bikeData={bikeData.bikeTripData} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
