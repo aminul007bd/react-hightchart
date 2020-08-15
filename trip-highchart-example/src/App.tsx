d061db08ab12a5b9dc76c7ef6833fe2ea108eefe
aminul007bd <gem007bd@gmail.com>
2020-08-15 12:44:02 +0200

    add highchart

--- trip-highchart-example/src/App.tsx
+++ trip-highchart-example/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import HighChartAverageTripofDay from './components/HighChartAverageTripofDay';
 import HelloWorld from './components/HelloWorld';
+im;
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
+      <HighChartAverageTripofDay />
       <HelloWorld />
     </div>
   );
